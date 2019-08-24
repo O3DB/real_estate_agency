93cb3c1c31d07f8ae84400da6d224d5ef327ed60
Oleg.Orlov <orlov.o@mail.ru>
2019-08-24 12:52:02 +0300

    Normalize Flat.phonenumbers into Flat.owner_phone_pure

--- property/migrations/0003_auto_20190820_2205.py
+++ property/migrations/0003_auto_20190820_2205.py
@@ -2,16 +2,12 @@
 
 from django.db import migrations
 
+
 def set_new_buildings(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
     Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
-    # for flat in Flat.objects.all():
-    #     if flat.construction_year >=2015:
-    #         flat.new_buiding = True
-    #     else:
-    #         flat.new_buiding = False
-    #     flat.save()
+
 
 class Migration(migrations.Migration):
 
