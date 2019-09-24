f623f7445d27097863021d1b10c126d97e8a5a00
Oleg.Orlov <orlov.o@mail.ru>
2019-09-24 20:23:36 +0300

    Fix data migration for new buildings

--- property/migrations/0003_auto_20190820_2205.py
+++ property/migrations/0003_auto_20190820_2205.py
@@ -6,7 +6,9 @@ from django.db import migrations
 def set_new_buildings(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
-    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
+    Flat.objects.filter(construction_year__lt=2015)\
+                .exclude(construction_year__isnull=True)\
+                .update(new_building=False)
 
 
 class Migration(migrations.Migration):
